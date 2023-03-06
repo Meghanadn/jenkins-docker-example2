9676116eafb76c3621712c318427b0f0e197a916
Meghana Nagaraj <92202681+Meghanadn@users.noreply.github.com>
2023-03-06 15:05:03 +0530

    Update apptest.java

--- src/est/java/com/mycompany/apptest.java
+++ src/est/java/com/mycompany/apptest.java
@@ -1,1 +1,20 @@
+ackage com.mycompany.app;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test 🙂
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
